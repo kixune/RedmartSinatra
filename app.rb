19a2fa975287773030b55603b8183f2c2c03c661
Barnabas Chan <barny@post.com>
2016-07-22 13:34:02 +0800

    define functions in app

--- app.rb
+++ app.rb
@@ -4,9 +4,46 @@ class RedmartSinatraApp < Sinatra::Base
     erb '<h1>annyeong</h1>'
   end
 
+  #get all users
   get '/users' do
     @users = User.all
     erb :'users'
   end
 
+  #get form view to create new user
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  #get one specific user
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    erb :'users/show'
+  end
+
+  #get form view to edit an existing user
+  get 'users/:id/edit' do
+    @user = User.find(params[:id])
+    erb :'users/edit'
+  end
+
+  #post user (creating new)
+  post '/users' do
+
+  end
+
+  #put user (update information)
+  put 'users/:id' do
+    @the_user = User.find(params[:id])
+    @the_user.update_attributes(
+      # key: "value"
+    )
+  end
+
+  #destroy user
+  delete 'users/:id' do
+    @deleted_user = User.find(params[:id])
+    @deleted_user.destroy
+  end
+  
 end
