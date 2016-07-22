ec60b52bd691f95c0607f746261a0114248533d4
Barnabas Chan <barny@post.com>
2016-07-22 13:46:24 +0800

    make base html for users new.erb, add new field in users table

--- db/migrate/20160721043653_create_users.rb
+++ db/migrate/20160721043653_create_users.rb
@@ -2,11 +2,12 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :user do |t|
 
-      t.string :name, null: false, limit: 30
+      t.string :first_name, null: false, limit: 30
+      t.string :last_name, null: false, limit: 30
       t.string :email, null: false, limit: 40
       t.string :address, null: false, limit: 50
       t.string :password, null: false, limit: 20
-      t.integer :cc_number
+      t.integer :cc_number limit 24
 
       t.timestamps
     end
